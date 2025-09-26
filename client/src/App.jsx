fc6770c9408d0e4d518fcd0fd5ddaccbc8f71001
Naman_ptdr <namanpatidar5555@gmail.com>
2025-09-26 10:33:20 +0530

    feat(client): initialize frontend with Vite + React
    - Created client folder using Vite
    - Installed React and dependencies
    - Verified initial setup with dev server

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h2> Sweets-Shop </h2>
+      </div>
+    </>
+  )
+}
+
+export default App
